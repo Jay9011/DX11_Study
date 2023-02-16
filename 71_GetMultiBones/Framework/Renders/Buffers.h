722cf9d9f606c536be2edc1aac6c5612140403b6
Jay9011 <whdtjq5904@gmail.com>
2023-02-16 19:54:07 +0900

    Modify Model Animator

--- 71_GetMultiBones/Framework/Renders/Buffers.h
+++ 71_GetMultiBones/Framework/Renders/Buffers.h
@@ -157,6 +157,10 @@ public:
     ID3D11Texture2D* Output() { return (ID3D11Texture2D*)output; }
     ID3D11ShaderResourceView* OutputSRV() { return outputSRV; }
 
+    void CopyToInput(ID3D11Texture2D* texture);
+    ID3D11Texture2D* CopyFromOutput();
+    ID3D11Texture2D* Result() { return (ID3D11Texture2D*)result; }
+
 private:
     UINT width, height, arraySize;
     DXGI_FORMAT format;
