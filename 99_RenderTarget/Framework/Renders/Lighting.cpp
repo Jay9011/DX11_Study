30fcbd16d4649a554701e8019f9f51fac0246eaf
Jay9011 <whdtjq5904@gmail.com>
2023-03-30 17:03:02 +0900

    Init Render Target and Depth Stencil

---
+++ 99_RenderTarget/Framework/Renders/Lighting.cpp
@@ -0,0 +1,69 @@
+﻿#include "Framework.h"
+#include "Lighting.h"
+
+Lighting* Lighting::instance = nullptr;
+
+Lighting* Lighting::Get()
+{
+	assert(instance != nullptr);
+
+	return instance;
+}
+
+void Lighting::Create()
+{
+	assert(instance == nullptr);
+
+	instance = new Lighting();
+}
+
+void Lighting::Delete()
+{
+	SafeDelete(instance);
+}
+
+Lighting::Lighting()
+{
+
+}
+
+Lighting::~Lighting()
+{
+
+}
+
+UINT Lighting::PointLights(OUT PointLight* lights)
+{
+	memcpy(lights, pointLights, sizeof(PointLight) * pointLightCount);
+
+	return pointLightCount;
+}
+
+void Lighting::AddPointLight(PointLight& light)
+{
+	pointLights[pointLightCount] = light;
+	pointLightCount++;
+}
+
+PointLight& Lighting::GetPointLight(UINT index)
+{
+	return pointLights[index];
+}
+
+UINT Lighting::SpotLights(OUT SpotLight * lights)
+{
+	memcpy(lights, spotLights, sizeof(SpotLight) * spotLightCount);
+
+	return spotLightCount;
+}
+
+void Lighting::AddSpotLight(SpotLight & light)
+{
+	spotLights[spotLightCount] = light;
+	spotLightCount++;
+}
+
+SpotLight & Lighting::GetSpotLight(UINT index)
+{
+	return spotLights[index];
+}
