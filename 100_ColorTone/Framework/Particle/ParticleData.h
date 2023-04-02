399e19c5f2acc8ebfb3b409e44d4efa5d893371f
Jay9011 <whdtjq5904@gmail.com>
2023-04-02 22:25:23 +0900

    Color Tone

---
+++ 100_ColorTone/Framework/Particle/ParticleData.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Framework.h"
+
+#include "Utilities/Xml.h"
+
+struct ParticleData
+{
+	enum class BlendType
+	{
+		Opaque = 0,
+		Additive,
+		AlphaBlend
+	} Type = BlendType::Opaque;
+	
+	wstring TextureFile;
+
+	UINT MaxParticles = 100;
+
+
+	bool bLoop = false;
+
+	float ReadyTime = 1.0f;
+	float ReadyRandomTime = 0;
+
+	float StartVelocity = 1;
+	float EndVelocity = 1;
+
+	float MinHorizontalVelocity = 0;
+	float MaxHorizontalVelocity = 0;
+
+	float MinVerticalVelocity = 0;
+	float MaxVerticalVelocity = 0;
+
+
+	Vector3 Gravity = Vector3(0, 0, 0);
+
+	// Random 한 Color 를 주기 위한 Min Max Color
+	Color MinColor = Color(1, 1, 1, 1);
+	Color MaxColor = Color(1, 1, 1, 1);
+
+	float ColorAmount = 1.0f;
+
+	float MinRotateSpeed = 0;
+	float MaxRotateSpeed = 0;
+
+	float MinStartSize = 100;
+	float MaxStartSize = 100;
+
+	float MinEndSize = 100;
+	float MaxEndSize = 100;
+};
---
+++ _Shaders/100_ColorTone.fx
@@ -0,0 +1,8 @@
+#include "00_Global.fx"
+#include "00_Light.fx"
+#include "00_Render.fx"
+
+technique11 T0
+{
+    P_VP(P0, VS_Mesh, PS)
+}
